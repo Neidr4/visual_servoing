b0c460013d2c0322906d978ee7181a160bad6793
Neidr4 <a.drapbiz@gmail.com>
2021-10-16 21:21:15 +0200

    Basic Feature detection
    This commit updates issue #11 and #13.
    It gathers the data from the video feed;
    Transform it into an OpenCV compatible format;
    Apply a mask on the color wanted;
    Detected the top and bottom coordinate of the color and assign it into points;
    Draw circles to locate the points on the image and display it.

--- src/visual_servoing_script.py
+++ src/visual_servoing_script.py
@@ -10,21 +10,30 @@ from sensor_msgs.msg import Image
 focal_length_px = 580
 
 class VS:
-    def __init__ (self, focal_length_px):
+    def __init__ (self, video_topic, focal_length_px):
         # Subscriber
-        self.sub_cam = rospy.Subscriber('/rms/camera1/image_raw', 
+        self.sub_cam = rospy.Subscriber(video_topic, 
             Image, self.callback_cam)
         # Variables
         self.bridge = CvBridge()
         self.cv_image = numpy.ndarray((480, 640, 4)) # Image()
+        self.mask = numpy.ndarray((480, 640, 4)) # Image()
+        self.image_with_circle = numpy.ndarray((480, 640, 4)) # Image()
         self.last_image = Image()
         self.triple_jacobian = numpy.empty((0, 6), float)
         self.focal_length_px = focal_length_px
 
+        #Colors
+        self.green_lower = numpy.array([40,30,30])
+        self.green_upper = numpy.array([70,255,255])
+        self.blue_lower = numpy.array([80, 30, 30])
+        self.blue_upper = numpy.array([120,255,255])
+        self.yellow_lower = numpy.array([30, 80, 100])
+        self.yellow_upper = numpy.array([60, 255, 255])
+
     def callback_cam(self, image_raw):
         try:
             self.cv_image = self.bridge.imgmsg_to_cv2(image_raw, "bgr8")
-            # self.display_image("bonsoir", self.cv_image)
         except CvBridgeError as e:
             print(e)
 
@@ -42,6 +51,47 @@ class VS:
                 rospy.loginfo("The argment passed in compute_ij, is not Point")
         print("robot_0.triple_jacobian size is: " + str(self.triple_jacobian.shape))
 
+    def get_features_pos(self):
+        hsv = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
+        #Detection of color in hsv image
+        self.mask = cv2.inRange(hsv, self.blue_lower, self.blue_upper)
+        
+        #Image display
+        #cv2.imshow("cv2_image", cv2_image)
+        #cv2.imshow("hsv", hsv)
+
+        point_1.set_uv_to_0()
+        point_2.set_uv_to_0()
+        point_3.set_uv_to_0()
+
+        mask_rows = self.mask.shape[0]
+        mask_columns = self.mask.shape[1]
+
+        for i in range(mask_rows):
+            for j in range(mask_columns):
+                
+                if(self.mask[i, j] != 0):
+                    if(point_1.u == 0 and point_1.v == 0):
+                        point_1.u = j
+                        point_1.v = i
+
+                    point_3.u = j
+                    point_3.v = i
+
+        point_2.u = (point_1.u + point_3.u )/2
+        point_2.v = (point_1.v + point_3.v )/2
+
+        point_1.print_coordinates()
+        point_2.print_coordinates()
+        point_3.print_coordinates()
+
+      #  self.mask = point_1.add_circle(self.mask)
+      #  self.mask = point_3.add_circle(self.mask)
+      #  cv2.imshow("get_features_pos", self.mask)
+      #  cv2.waitKey(0)
+      #  cv2.destroyAllWindows()
+
+
     def clear_jacobian(self):
         self.triple_jacobian = numpy.empty((0, 6), float)
 
@@ -49,6 +99,10 @@ class VS:
         cv2.imshow(name, img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+      #  if cv2.waitKey(33) == ord('q'):
+      #      cv2.destroyAllWindows()
+      #  else:
+      #      print("press \"q\" for closing the window")
 
     def shutdown_function(self):
         # cv2.waitKey(0)
@@ -57,37 +111,42 @@ class VS:
 
 class Point(object):
     """docstring for Point"""
-    def __init__(self, name, u, v, Z):
+    def __init__(self, name, u, v, Z, color):
         super(Point, self).__init__()
         self.name = name
         self.u = u
         self.v = v
         self.Z = Z
+        self.color = color
 
     def print_coordinates(self):
         print("Coordinates of "+str(self.name)+" is (" 
             +str(self.u)+", "+str(self.v)+", "+str(self.Z)+")")
 
+    def set_uv_to_0(self):
+        self.u = 0
+        self.v = 0
+
     def add_circle(self, img):
         # Center coordinates
         center_coordinates_check = [self.u, self.v]
 
-        if(self.u > img.shape[1]):
-            center_coordinates_check[0] = img.shape[1]
-        else:
-            center_coordinates_check[0] = self.u
-        
-        if(self.u > img.shape[0]):
-            center_coordinates_check[1] = img.shape[0]
-        else:
-            center_coordinates_check[1] = self.v
+      #  if(self.u > img.shape[1]):
+      #      center_coordinates_check[0] = img.shape[1]
+      #  else:
+      #      center_coordinates_check[0] = self.u
+      #  
+      #  if(self.u > img.shape[0]):
+      #      center_coordinates_check[1] = img.shape[0]
+      #  else:
+      #      center_coordinates_check[1] = self.v
 
         center_coordinates = tuple(center_coordinates_check)
 
         # Radius of circle
         radius = 20
         # Blue color in BGR
-        color = (255, 0, 0)
+        color = self.color
         # Line thickness of 2 px
         thickness = 2
           
@@ -98,8 +157,8 @@ class Point(object):
 
 
 def get_param():
-    global global_topic_name
-    global_topic_name = rospy.get_param("/visual_servoing_node/video_feed_topic")
+    global video_feed_topic
+    video_feed_topic = rospy.get_param("/visual_servoing_node/video_feed_topic")
     global focal_length_pxl
     focal_length_pxl = rospy.get_param("/visual_servoing_node/focal_length_pxl")
     global padding_horizontal
@@ -120,20 +179,22 @@ if __name__ == '__main__':
 
     rospy.loginfo("Fetching parameters")
     get_param()
-    print("global_topic_name is: " + str(global_topic_name))
-    print("focal_length_pxl is: " + str(focal_length_pxl))
-    print("padding_horizontal is: " + str(padding_horizontal))
-    print("padding_vertical is: " + str(padding_vertical))
 
     rospy.loginfo("Creation of instance of VS")
-    robot_0 = VS(focal_length_px=580)
+    robot_0 = VS(video_topic=video_feed_topic, 
+        focal_length_px=focal_length_pxl)
     rospy.on_shutdown(shutdown_function)
 
     rospy.loginfo("Creation of three instance of Point")
-    point_1 = Point("Point_1", 200, 200, 200)
-    point_2 = Point("Point_2", 400, 200, 200)
-    point_3 = Point("Point_3", 300, 400, 200)
+
+    desired_point_1 = Point("Desired_Point_1", 200, 200, 200, (255, 0, 255))
+    desired_point_2 = Point("Desired_Point_2", 400, 200, 200, (0, 255, 255))
+    desired_point_3 = Point("Desired_Point_3", 300, 400, 200, (255, 255, 0))
+    point_1 = Point("Point_1", 200, 200, 200, (0, 0, 255))
+    point_2 = Point("Point_2", 400, 200, 200, (0, 255, 0))
+    point_3 = Point("Point_3", 300, 400, 200, (255, 0, 0))
     list_of_point = [point_1, point_2, point_3]
+    first_try = 0
 
 
     rospy.loginfo("Entering loop")
@@ -141,10 +202,22 @@ if __name__ == '__main__':
         try:
             rospy.loginfo("Start of Cycle")
 
-            point_1.add_circle(robot_0.cv_image)
-            point_2.add_circle(robot_0.cv_image)
-            point_3.add_circle(robot_0.cv_image)
-            robot_0.display_image("robot_0", robot_0.cv_image)
+            if first_try == 0:
+                first_try = 1
+            else:
+                robot_0.get_features_pos()
+
+            robot_0.image_with_circle = point_1.add_circle(robot_0.cv_image)
+            robot_0.image_with_circle = point_2.add_circle(robot_0.cv_image)
+            robot_0.image_with_circle = point_3.add_circle(robot_0.cv_image)
+
+            robot_0.mask = point_1.add_circle(robot_0.cv_image)
+            robot_0.mask = point_2.add_circle(robot_0.cv_image)
+            robot_0.mask = point_3.add_circle(robot_0.cv_image)
+
+            robot_0.display_image("robot_0_mask", robot_0.mask)
+            robot_0.display_image("robot_0_image_with_circle", 
+                robot_0.image_with_circle)
 
             robot_0.compute_ij(list_of_point)
 
@@ -152,4 +225,5 @@ if __name__ == '__main__':
             rospy.loginfo("End of Cycle")
             # rospy.spin()
         except rospy.ROSInterruptException:
+            shutdown_function()
             pass
